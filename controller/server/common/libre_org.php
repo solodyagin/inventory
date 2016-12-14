bceecb1f2e9ec8ad0255b777fd3fdfa291a96145
solodyagin <solodyagin@gmail.com>
2016-12-14 18:00:09 +0300

    Исправил ошибку получения списка организаций

--- controller/server/common/libre_org.php
+++ controller/server/common/libre_org.php
@@ -24,6 +24,14 @@ if ($oper == '') {
 	// Проверяем может ли пользователь просматривать?
 	(($user->mode == 1) || $user->TestRoles('1,3,4,5,6')) or die('Недостаточно прав');
 
+	// Готовим ответ
+	$responce = new stdClass();
+	$responce->page = 0;
+	$responce->total = 0;
+	$responce->records = 0;
+
+	$count = 0;
+
 	$sql = 'SELECT COUNT(*) AS cnt FROM org';
 	try {
 		$row = DB::prepare($sql)->execute()->fetch();
@@ -34,37 +42,37 @@ if ($oper == '') {
 		throw new DBException('Не могу выбрать список организаций', 0, $ex);
 	}
 
-	$total_pages = ($count > 0) ? ceil($count / $limit) : 0;
-	if ($page > $total_pages) {
-		$page = $total_pages;
-	}
-	$start = $limit * $page - $limit;
-
-	$responce = new stdClass();
-	$responce->page = $page;
-	$responce->total = $total_pages;
-	$responce->records = $count;
-
-	$sql = 'SELECT id, name, active, picmap FROM org ORDER BY :sidx :sord LIMIT :start, :limit';
-	try {
-		$arr = DB::prepare($sql)->execute(array(
-					':sidx' => $sidx,
-					':sord' => $sord,
-					':start' => $start,
-					':limit' => $limit
-				))->fetchAll();
-		$i = 0;
-		foreach ($arr as $row) {
-			$responce->rows[$i]['id'] = $row['id'];
-			if ($row['active'] == '1') {
-				$responce->rows[$i]['cell'] = array('<i class="fa fa-check-circle-o" aria-hidden="true"></i>', $row['id'], $row['name']);
-			} else {
-				$responce->rows[$i]['cell'] = array('<i class="fa fa-ban" aria-hidden="true"></i>', $row['id'], $row['name']);
-			}
-			$i++;
+	if ($count > 0) {
+		$total_pages = ceil($count / $limit);
+		if ($page > $total_pages) {
+			$page = $total_pages;
 		}
-	} catch (PDOException $ex) {
-		throw new DBException('Не могу выбрать список организаций', 0, $ex);
+		$start = $limit * $page - $limit;
+		if ($start < 0) {
+			jsonExit($responce);
+		}
+
+		$responce->page = $page;
+		$responce->total = $total_pages;
+		$responce->records = $count;
+
+		$sql = "SELECT id, name, active, picmap FROM org ORDER BY $sidx $sord LIMIT $start, $limit";
+		try {
+			$arr = DB::prepare($sql)->execute()->fetchAll();
+			$i = 0;
+			foreach ($arr as $row) {
+				$responce->rows[$i]['id'] = $row['id'];
+				$ic = ($row['active'] == '1') ? 'fa-check-circle-o' : 'fa-ban';
+				$responce->rows[$i]['cell'] = array(
+					"<i class=\"fa $ic\" aria-hidden=\"true\"></i>",
+					$row['id'],
+					$row['name']
+				);
+				$i++;
+			}
+		} catch (PDOException $ex) {
+			throw new DBException('Не могу выбрать список организаций', 0, $ex);
+		}
 	}
 	jsonExit($responce);
 }
